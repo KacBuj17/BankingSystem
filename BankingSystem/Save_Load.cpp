fb83550a231727f9c72dda385510c9cfcfe17add
KacBuj17 <kbujak150@interia.pl>
2023-12-10 21:01:14 +0100

    NowaWersja

--- BankingSystem/Save_Load.cpp
+++ BankingSystem/Save_Load.cpp
@@ -1,43 +1,43 @@
-#pragma once
-#include "Save_Load.h"
-using namespace std;
-
-template <typename T> vector<T> Save_Load<T>::Load(const string& fileName)
-{
-    vector<T> objects;
-    ifstream file(fileName + ".txt");
-
-    if (file.is_open())
-    {
-        T object{};
-        while (file >> object)
-        {
-            objects.push_back(object);
-        }
-        file.close();
-    }
-    else
-    {
-        cout << "Nie udalo sie otworzyc pliku!" << std::endl;
-    }
-
-    return objects;
-}
-
-template <typename T> void Save_Load<T>::Save(const vector<T>& objects, const std::string& fileName)
-{
-    ofstream file(fileName + ".txt");
-
-    if (file.is_open())
-    {
-        for (const auto& object : objects)
-        {
-            file << object << std::endl;
-        }
-        file.close();
-    }
-    else
-    {
-        cout << "Nie udalo sie otworzyc pliku!" << std::endl;
-    }
-}
+//#pragma once
+//#include "Save_Load.h"
+//using namespace std;
+//
+//template <typename T> vector<T> Save_Load<T>::Load(const string& fileName)
+//{
+//    vector<T> objects;
+//    ifstream file(fileName + ".txt");
+//
+//    if (file.is_open())
+//    {
+//        T object{};
+//        while (file >> object)
+//        {
+//            objects.push_back(object);
+//        }
+//        file.close();
+//    }
+//    else
+//    {
+//        cout << "Nie udalo sie otworzyc pliku!" << std::endl;
+//    }
+//
+//    return objects;
+//}
+//
+//template <typename T> void Save_Load<T>::Save(const vector<T>& objects, const std::string& fileName)
+//{
+//    ofstream file(fileName + ".txt");
+//
+//    if (file.is_open())
+//    {
+//        for (const auto& object : objects)
+//        {
+//            file << object << std::endl;
+//        }
+//        file.close();
+//    }
+//    else
+//    {
+//        cout << "Nie udalo sie otworzyc pliku!" << std::endl;
+//    }
+//}
--- BankingSystem/Save_Load.h
+++ BankingSystem/Save_Load.h
@@ -8,8 +8,43 @@ using namespace std;
 template <typename T> class Save_Load
 {
 public:
-    static vector<T> Load(const std::string& fileName);
-    static void Save(const vector<T>& objects, const std::string& fileName);
-};
+    static vector<T> Load(const string& fileName)
+    {
+        vector<T> objects;
+        ifstream file(fileName + ".txt");
 
-#include "Save_Load.cpp"
+        if (file.is_open())
+        {
+            T object{};
+            while (file >> object)
+            {
+                objects.push_back(object);
+            }
+            file.close();
+        }
+        else
+        {
+            cout << "Nie udalo sie otworzyc pliku!" << std::endl;
+        }
+
+        return objects;
+    }
+
+    static void Save(const vector<T>& objects, const std::string& fileName)
+    {
+        ofstream file(fileName + ".txt");
+
+        if (file.is_open())
+        {
+            for (const auto& object : objects)
+            {
+                file << object << std::endl;
+            }
+            file.close();
+        }
+        else
+        {
+            cout << "Nie udalo sie otworzyc pliku!" << std::endl;
+        }
+    }
+};
